236ff6aa5e8f34979b6149e136cb0c0869c35070
Jorgen Skogmo <js@dearstudio.dk>
2024-10-08 08:46:17 +0200

    ui

--- code.ts
+++ code.ts
@@ -2,6 +2,7 @@
 
 const TOKEN_PREFIX: string = "atomlib";
 const NUMBER_UNIT: string = ""; //"px";
+const VARIABLE_COLLECTION_NAME = "theme";
 
 type LimitedVariableValue = boolean | string | number | RGB | RGBA;
 
@@ -11,6 +12,14 @@ interface TokenType {
 	type: VariableResolvedDataType;
 	value: LimitedVariableValue;
 }
+
+figma.ui.onmessage = (message) => {
+	// console.log("got this from the UI", message);
+	if (message.type === "dismiss") {
+		figma.ui.close();
+		figma.closePlugin();
+	}
+};
 
 function rgbToHex({ r, g, b, a }: RGBA) {
 	if (a !== 1) {
@@ -30,6 +39,9 @@ function rgbToHex({ r, g, b, a }: RGBA) {
 const isAlias = (v: VariableValue): boolean => {
 	return JSON.stringify(v).includes("VARIABLE_ALIAS");
 };
+
+// todo: align with Semantic Tokens:
+// --figma-color-{type}-{color role}-{prominence}-{interaction}
 
 const buildToken = ({ name, type, mode, value }: TokenType): string => {
 	// console.log("buildToken:", name, type, mode, value);
@@ -61,7 +73,7 @@ const buildToken = ({ name, type, mode, value }: TokenType): string => {
 	return `--${prefix}${_mode}-${_type}-${_name}: ${_value}; ${comment !== "" ? `/* ${comment} */` : ""}`;
 };
 
-async function export_tokens() {
+async function main() {
 	const localVariables = await figma.variables.getLocalVariablesAsync();
 	// console.log("localVariables:", localVariables);
 
@@ -70,105 +82,122 @@ async function export_tokens() {
 
 	// Find the "theme" collection
 	const themeCollection = collections.find(
-		(collection) => collection.name === "theme",
+		(collection) => collection.name === VARIABLE_COLLECTION_NAME,
 	);
 
 	if (!themeCollection) {
-		// todo: show ui message
-		console.error("Theme collection not found");
-		return;
+		console.error(`${VARIABLE_COLLECTION_NAME} collection not found`);
+
+		// show ui message
+		figma.showUI(__html__, {
+			visible: true,
+			themeColors: true,
+			width: 600,
+			height: 300,
+		});
+		figma.ui.postMessage({
+			type: "MESSAGE",
+			payload: `<h1>Error: "${VARIABLE_COLLECTION_NAME} collection" missing</h1>
+			<p>This exporter works by exporting a variable collection named "${VARIABLE_COLLECTION_NAME}".
+			<br />
+			Please restructure your variables and then run the plug-in again.</p>`,
+		});
 	}
 
-	// Get all variables in the theme collection
-	const themeVariables = localVariables.filter(
-		(v) => v.variableCollectionId === themeCollection.id,
-	);
-	// console.log("themeVariables:", themeVariables);
+	if (themeCollection) {
+		// and...
 
-	const resolveVariable = (variableId: string, depth = 0): VariableValue => {
-		// console.log("~ resolveVariable", variableId, depth);
-		const variable = localVariables.find((v) => v.id === variableId);
-		if (depth > 100) {
-			console.error("Too many recursions");
-			return -100;
-		}
-		if (!variable) {
-			console.error("Variable not found");
-			return -10;
-		}
+		// Get all variables in the theme collection
+		const themeVariables = localVariables.filter(
+			(v) => v.variableCollectionId === themeCollection.id,
+		);
+		// console.log("themeVariables:", themeVariables);
 
-		// Determine if this is an alias
-		const value = Object.values(variable.valuesByMode)[0]; // only the "theme" collection should have columns
-
-		// console.log("~~ isAlias:", isAlias(value));
-
-		if (isAlias(value)) {
-			const alias = value as { type: "VARIABLE_ALIAS"; id: string };
-			return resolveVariable(alias.id, depth + 1);
-		}
-
-		return value;
-	};
-
-	const tokens: string[] = [];
-
-	// loop through all themeVariables
-	for (const variable of themeVariables) {
-		// console.log("theme variable:", variable.name, variable);
-
-		const name = variable.name;
-		const type = variable.resolvedType;
-
-		/// ...and their valuesByModes (Figma: "variable mode" (e.g. columns in the variable editor)
-		const valuesByModeKeys = Object.keys(variable.valuesByMode);
-		for (const vmk of valuesByModeKeys) {
-			const variant = variable.valuesByMode[vmk];
-
-			/// find their raw value (recursively traverse the alias-chain)
-			let rawValue = variant;
-			if (isAlias(variant)) {
-				const alias = variant as { type: "VARIABLE_ALIAS"; id: string };
-				rawValue = resolveVariable(alias.id);
+		const resolveVariable = (variableId: string, depth = 0): VariableValue => {
+			// console.log("~ resolveVariable", variableId, depth);
+			const variable = localVariables.find((v) => v.id === variableId);
+			if (depth > 100) {
+				console.error("Too many recursions");
+				return -100;
+			}
+			if (!variable) {
+				console.error("Variable not found");
+				return -10;
 			}
 
-			/// no aliases from here on
-			let value = <LimitedVariableValue>rawValue;
+			// Determine if this is an alias
+			const value = Object.values(variable.valuesByMode)[0]; // only the "theme" collection should have columns
 
-			if (variable.resolvedType === "COLOR") {
-				value = rgbToHex(value as RGBA);
+			// console.log("~~ isAlias:", isAlias(value));
+
+			if (isAlias(value)) {
+				const alias = value as { type: "VARIABLE_ALIAS"; id: string };
+				return resolveVariable(alias.id, depth + 1);
 			}
 
-			const _modeName = themeCollection.modes.find(
-				(mode) => mode.modeId === vmk,
-			);
-			const mode = _modeName ? _modeName.name : "default";
+			return value;
+		};
 
-			// console.log(
-			// 	"T",
-			// 	name,
-			// 	mode,
-			// 	type,
-			// 	value,
-			// );
+		const tokens: string[] = [];
 
-			tokens.push(
-				buildToken({
-					name,
-					type,
-					mode,
-					value,
-				}),
-			);
+		// loop through all themeVariables
+		for (const variable of themeVariables) {
+			// console.log("theme variable:", variable.name, variable);
+
+			const name = variable.name;
+			const type = variable.resolvedType;
+
+			/// ...and their valuesByModes (Figma: "variable mode" (e.g. columns in the variable editor)
+			const valuesByModeKeys = Object.keys(variable.valuesByMode);
+			for (const vmk of valuesByModeKeys) {
+				const variant = variable.valuesByMode[vmk];
+
+				/// find their raw value (recursively traverse the alias-chain)
+				let rawValue = variant;
+				if (isAlias(variant)) {
+					const alias = variant as { type: "VARIABLE_ALIAS"; id: string };
+					rawValue = resolveVariable(alias.id);
+				}
+
+				/// no aliases from here on
+				let value = <LimitedVariableValue>rawValue;
+
+				if (variable.resolvedType === "COLOR") {
+					value = rgbToHex(value as RGBA);
+				}
+
+				const _modeName = themeCollection.modes.find(
+					(mode) => mode.modeId === vmk,
+				);
+				const mode = _modeName ? _modeName.name : "default";
+
+				// console.log(
+				// 	"T",
+				// 	name,
+				// 	mode,
+				// 	type,
+				// 	value,
+				// );
+
+				tokens.push(
+					buildToken({
+						name,
+						type,
+						mode,
+						value,
+					}),
+				);
+			}
 		}
+
+		const sorted_tokens = tokens.sort();
+		// console.log("tokens:", sorted_tokens);
+
+		figma.showUI(__html__, { visible: false });
+		figma.ui.postMessage({ type: "EXPORT_RESULT", payload: sorted_tokens });
+
+		setTimeout(() => figma.closePlugin(), 1000);
 	}
-
-	const sorted_tokens = tokens.sort();
-	// console.log("tokens:", sorted_tokens);
-
-	figma.showUI(__html__, { visible: false });
-	figma.ui.postMessage({ type: "EXPORT_RESULT", tokens: sorted_tokens });
-
-	setTimeout(() => figma.closePlugin(), 1000);
 }
 
-export_tokens();
+main();
